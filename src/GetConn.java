1129c9caf45512dd4ff0002d25bd59a637de3a9f
maocili <maocili@163.com>
2018-06-26 22:57:06 +0800

    新增注册窗体、主窗体

--- src/GetConn.java
+++ src/GetConn.java
@@ -1,9 +1,14 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
 
 public class GetConn {
-	Connection conn;
-	public Connection getConnection() {
+	static Connection conn;
+	public static Connection getConnection() {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			conn=DriverManager.getConnection("jdbc:sqlserver://web.maocil.cn:1433;DatabaseName=student_system","admin","990219Maocil");
--- src/Login.java
+++ src/Login.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -30,7 +32,7 @@ public class Login extends JFrame {
 	Point loc = null;
 	Point tmp = null;
 	boolean isDragged = false;
-	
+	static Login frame = new Login();
 	
 	GetConn getConn=new GetConn();
 	
@@ -41,7 +43,7 @@ public class Login extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login frame = new Login();
+					frame = new Login();
 					frame.setVisible(true);
 					frame.setLocationRelativeTo(null);
  
@@ -65,11 +67,58 @@ public class Login extends JFrame {
 		this.setUndecorated(true); // »•µÙ¥∞ø⁄µƒ◊∞ Œ  
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				login();
+			}
+
+			private void login() {
+				Connection conn;
+				try {
+					
+					conn=getConn.getConnection();
+//					SQL¡¨Ω””Ôæ‰
+					PreparedStatement statement=conn.prepareStatement(
+							"select * from account where student_id=? and password=?");
+					statement.setString(1, textField_1.getText());
+					statement.setString(2, new String(passwordField.getPassword()));
+					ResultSet result=statement.executeQuery();
+					/*
+					 * ≈–∂œ «∑Ò¥Ê‘⁄’À∫≈√‹¬Î
+					 * */
+					if(result.next()) {
+						
+						main_frame main_f = new main_frame();
+						main_f.setVisible(true);
+						main_f.setLocationRelativeTo(null);
+						frame.setVisible(false);
+						
+						
+						
+					}else {
+						showMessageFrame showMsg = new showMessageFrame("µ«¬Ω ß∞‹");
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+				btnNewButton.setIcon(new ImageIcon(".\\Images\\login.png"));
+				btnNewButton.setBounds(144, 301, 57, 57);
+				btnNewButton.setContentAreaFilled(false);
+				btnNewButton.setFocusPainted(false);
+				btnNewButton.setBorderPainted(false);
+				contentPane.add(btnNewButton);
 		
 		
 			
 		passwordField = new JPasswordField();
 		passwordField.setBounds(119, 246, 162, 32);
+		passwordField.setOpaque(false);
+		passwordField.setBorder(new EmptyBorder(0,0,0,0));
 		contentPane.add(passwordField);
 		/*
 		 * textField_1  username text
@@ -77,6 +126,8 @@ public class Login extends JFrame {
 		textField_1 = new JTextField();
 		textField_1.setForeground(Color.LIGHT_GRAY);
 		textField_1.setBounds(119, 177, 162, 32);
+		textField_1.setOpaque(false);
+		textField_1.setBorder(new EmptyBorder(0,0,0,0));
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		textField_1.addFocusListener(new FocusListener() {
@@ -109,18 +160,28 @@ public class Login extends JFrame {
 		label.setBounds(55, 180, 66, 24);
 		contentPane.add(label);
 		
+		JLabel label_4 = new JLabel("______________________");
+		label_4.setBounds(119, 194, 162, 15);
+		contentPane.add(label_4);
+		
 		JLabel label_1 = new JLabel("\u5BC6  \u7801\uFF1A");
 		label_1.setFont(new Font("”◊‘≤", Font.BOLD, 13));
 		label_1.setBounds(55, 249, 66, 24);
 		contentPane.add(label_1);
 		
+		JLabel label_5 = new JLabel("______________________");
+		label_5.setBounds(119, 266, 162, 15);
+		contentPane.add(label_5);
+		
 		JButton button = new JButton("");
-		button.setIcon(new ImageIcon(".\\Images\\x.png"));
-		button.setBounds(312, 2, 26, 25);
+		button.setIcon(new ImageIcon(".\\Images\\exit.png"));
+		button.setBounds(318, 0, 20, 20);
 		button.setContentAreaFilled(false);
 		button.setFocusPainted(false);
 		button.setBorderPainted(false);
 		contentPane.add(button);
+		
+		
 		
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon(".\\Images\\2.png"));
@@ -130,45 +191,6 @@ public class Login extends JFrame {
 		/*
 		 * Login_btn
 		 * */
-		JButton btnNewButton = new JButton("");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				login();
-			}
-
-			private void login() {
-				Connection conn;
-				try {
-					
-					conn=getConn.getConnection();
-//					SQL¡¨Ω””Ôæ‰
-					PreparedStatement statement=conn.prepareStatement(
-							"select * from account where student_id=? and password=?");
-					statement.setString(1, textField_1.getText());
-					statement.setString(2, new String(passwordField.getPassword()));
-					ResultSet result=statement.executeQuery();
-					/*
-					 * ≈–∂œ «∑Ò¥Ê‘⁄’À∫≈√‹¬Î
-					 * */
-					if(result.next()) {
-						System.out.println("1");
-						
-					}else {
-						System.out.println("2");
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				
-			}
-		});
-
-		btnNewButton.setIcon(new ImageIcon(".\\Images\\login.png"));
-		btnNewButton.setBounds(144, 301, 57, 57);
-		btnNewButton.setContentAreaFilled(false);
-		btnNewButton.setFocusPainted(false);
-		btnNewButton.setBorderPainted(false);
-		contentPane.add(btnNewButton);
 		
 		JButton button_1 = new JButton("\u627E\u56DE\u5BC6\u7801");
 		button_1.setBounds(217, 383, 93, 23);
@@ -177,16 +199,30 @@ public class Login extends JFrame {
 		button_1.setBorderPainted(false);
 		contentPane.add(button_1);
 		
-		JButton btnNewButton_1 = new JButton("\u8D26\u53F7\u6CE8\u518C");
-		btnNewButton_1.setBounds(28, 383, 93, 23);
-		btnNewButton_1.setContentAreaFilled(false);
-		btnNewButton_1.setFocusPainted(false);
-		btnNewButton_1.setBorderPainted(false);
-		contentPane.add(btnNewButton_1);
+		JButton register_btn = new JButton("\u8D26\u53F7\u6CE8\u518C");
+		register_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		register_btn.setBounds(28, 383, 93, 23);
+		register_btn.setContentAreaFilled(false);
+		register_btn.setFocusPainted(false);
+		register_btn.setBorderPainted(false);
+		contentPane.add(register_btn);
+		register_btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				register_frame register_f = new register_frame();
+				register_f.setVisible(true);
+				register_f.setLocationRelativeTo(null);
+				frame.setVisible(false);
+				
+			}
+		});
+		
 		
 		JLabel label_3 = new JLabel("");
 		label_3.setIcon(new ImageIcon(".\\Images\\break.png"));
-		label_3.setBounds(0, 0, 338, 480);
+		label_3.setBounds(0, 0, 340, 480);
 		contentPane.add(label_3);
 		
 		button.addActionListener(new ActionListener() {
@@ -195,6 +231,39 @@ public class Login extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				System.exit(0);
+			}
+		});
+		
+		button.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				button.setIcon(new ImageIcon(".\\Images\\exit.png"));
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				button.setIcon(new ImageIcon(".\\Images\\exit2.png"));
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
 			}
 		});
 		
